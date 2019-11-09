9f8546b14f6737bf6c0e173d91c2677e1369ff5a
John Boccio <john.t.boccio@gmail.com>
2019-11-09 12:53:13 -0500

    Implemented VGG for Fer2013

--- Main.py
+++ Main.py
@@ -1,24 +1,30 @@
-import ConfigParser as Cp
+from torch.utils import data
+import data_loader as dl
 import neural_nets as nns
 import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.optim as optim
+import utils as util
 
 
-main = Cp.ConfigParser.get_config()["main"]
-to_run = main["run"]
-task = main["task"]
+# TODO: Add arg parser to configure what NN is used and assign it a task
 
-if to_run["dataset"] == "fer2013":
-    net = None
-    transform = None
-    if to_run["model"] == "Fer2013V1":
-        transform = transforms.Compose(
-            [transforms.RandomHorizontalFlip(),
+net = nns.VggVdFaceFerDag()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.005, momentum=0.9)
+
+transform = transforms.Compose(
+            [transforms.Resize(256),
+             transforms.CenterCrop(224),
              transforms.ToTensor(),
-             transforms.Normalize(mean=[0.5], std=[0.5])]
-        )
-        net = nns.Fer2013V1(tf=transform)
-    else:
-        print("Invalid config.json file for main")
-        exit()
+             lambda x: x*255,
+             transforms.Normalize(mean=[129.186279296875, 104.76238250732422, 93.59396362304688], std=[1, 1, 1])])
 
-    nns.fer2013_test_nn(net)
+trainset = dl.FER2013Dataset(train=True, tf=transform)
+trainloader = data.DataLoader(trainset, batch_size=4,
+                              shuffle=True, num_workers=0)
+testset = dl.FER2013Dataset(train=False, tf=transform)
+testloader = data.DataLoader(testset, batch_size=4,
+                             shuffle=True, num_workers=0)
+
+util.fer2013_test_nn(net, testloader)
