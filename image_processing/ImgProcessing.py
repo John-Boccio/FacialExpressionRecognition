32554d4ba0c8d266c462caee49b9fe91e29f01b1
MuhammadHussain3 <hmuhammad9259@bths.edu>
2020-01-15 14:27:49 -0500

    Add files via upload

--- image_processing/ImgProcessing.py
+++ image_processing/ImgProcessing.py
@@ -8,14 +8,15 @@ Description:
 """
 from PIL import Image
 import cv2
-import numpy
+import numpy as np
+import os
 
 
 def crop_faces(image):
     face_data = "image_processing/resources/haarcascade_frontalface_default.xml"
     cascade = cv2.CascadeClassifier(face_data)
 
-    opencv_img = cv2.cvtColor(numpy.asarray(image).astype('uint8'), cv2.COLOR_RGB2BGR)
+    opencv_img = cv2.cvtColor(np.asarray(image).astype('uint8'), cv2.COLOR_RGB2BGR)
     minisize = (opencv_img.shape[1], opencv_img.shape[0])
     miniframe = cv2.resize(opencv_img, minisize)
 
@@ -35,9 +36,27 @@ def crop_faces(image):
     return crops
 
 
+def histogram_equalization(image):
+    img = cv2.cvtColor(np.asarray(image).astype('uint8'), cv2.COLOR_RGB2BGR)
+    img_yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
+    # equalize the histogram of the Y channel
+    img_yuv[:, :, 0] = cv2.equalizeHist(img_yuv[:, :, 0])
+    HE = []
+    # convert the YUV image back to RGB format
+    img_output = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
+    img_output= Image.fromarray(cv2.cvtColor(img_output, cv2.COLOR_BGR2RGB))
+    equalization = {
+        "img": img_output
+    }
+    HE.append(equalization)
+
+    return HE
+
+
 def crop_face_transform(image):
     faces = crop_faces(image)
-    if len(faces) == 0:
-        return image
-    return faces[0]["img"]
+    faces2 = histogram_equalization(faces)
+    if len(faces2) == 0:
+        return faces
+    return faces2[0]["img"]
 
