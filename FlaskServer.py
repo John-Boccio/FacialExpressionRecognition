8f6c3993810e78c68bd517c8d121150c59298f64
MuhammadHussain3 <hmuhammad9259@bths.edu>
2020-03-24 16:45:35 -0400

    Update FlaskServer.py
    Added image processing

--- FlaskServer.py
+++ FlaskServer.py
@@ -4,12 +4,17 @@ import threading
 import datetime
 import imutils
 import cv2
+from image_processing import histogram_equalization
+from image_processing import crop_faces
+from image_processing import crop_face_transform
 
 # initialize the output frame and a lock used to ensure thread-safe
 # exchanges of the output frames (useful for multiple browsers/tabs
 # are viewing the stream)
 outputFrame = None
 video_lock = threading.Lock()
+# initialize the video stream and allow the camera sensor to
+# warmup
 video = VideoStream(src=0).start()
 app = Flask(__name__)
 
@@ -28,14 +33,19 @@ def capture_frames():
     # loop over frames from the video stream
     while True:
         # read the next frame from the video stream, resize it,
-        # convert the frame to grayscale, and blur it
+        # apply face crop and histogram equalization
         frame = video.read()
-        frame = imutils.resize(frame, width=400)
+        # frame = imutils.resize(frame, width=400)
+        frame = crop_face_transform(frame)
+        frame = histogram_equalization(frame)
+
+
+
 
         # grab the current timestamp and draw it on the frame
         timestamp = datetime.datetime.now()
         cv2.putText(frame, timestamp.strftime(
-            "%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
+            "%A %d %B %Y %I:%M:%S%p"), (6, frame.shape[0] - 10),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
 
         # acquire the lock, set the output frame, and release the
