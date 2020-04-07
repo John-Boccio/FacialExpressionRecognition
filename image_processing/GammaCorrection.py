7302d5f1f11bd5f51fffee6a77353f53647d78a1
MuhammadHussain3 <hmuhammad9259@bths.edu>
2020-04-07 15:44:51 -0400

    added to Image Processing

--- image_processing/GammaCorrection.py
+++ image_processing/GammaCorrection.py
@@ -7,7 +7,8 @@ import numpy as np
 import argparse
 import cv2
 
-def gammaCorrection (image):
+
+def gamma_correction(image):
     def adjust_gamma(image, gamma=1.0):
         # build a lookup table mapping the pixel values [0, 255] to
         # their adjusted gamma values
@@ -17,7 +18,6 @@ def gammaCorrection (image):
 
         # apply gamma correction using the lookup table
         return cv2.LUT(image, table)
-
 
     # construct the argument parse and parse the arguments
     ap = argparse.ArgumentParser()
@@ -38,7 +38,7 @@ def gammaCorrection (image):
         gamma = gamma if gamma > 0 else 0.1
         adjusted = adjust_gamma(original, gamma=gamma)
     return adjusted
-    #cv2.putText(adjusted, "g={}".format(gamma), (10, 30),
-                #cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
-    #cv2.imshow("Images", np.hstack([original, adjusted]))
-    #cv2.waitKey(0)
+    # cv2.putText(adjusted, "g={}".format(gamma), (10, 30),
+    # cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
+    # cv2.imshow("Images", np.hstack([original, adjusted]))
+    # cv2.waitKey(0)
--- image_processing/ImgProcessing.py
+++ image_processing/ImgProcessing.py
@@ -6,6 +6,9 @@ Last revision:
 Description:
    Provides image processing functions.
 """
+from __future__ import print_function
+import numpy as np
+import argparse
 import cv2
 
 
@@ -43,6 +46,38 @@ def histogram_equalization(image):
     img_output = cv2.cvtColor(img_output, cv2.COLOR_BGR2RGB)
 
     return img_output
+
+
+def gamma_correction(image):
+    def adjust_gamma(gamma=1.0):
+        # build a lookup table mapping the pixel values [0, 255] to
+        # their adjusted gamma values
+        invGamma = 1.0 / gamma
+        table = np.array([((i / 255.0) ** invGamma) * 255
+                          for i in np.arange(0, 256)]).astype("uint8")
+
+        # apply gamma correction using the lookup table
+        return cv2.LUT(image, table)
+
+    # construct the argument parse and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", required=True,
+                    help="path to input image")
+    args = vars(ap.parse_args())
+
+    # load the original image
+    original = cv2.imread(args["image"])
+
+    # loop over various values of gamma
+    for gamma in np.arange(0.0, 3.5, 0.5):
+        # ignore when gamma is 1 (there will be no change to the image)
+        if gamma == 1:
+            continue
+
+        # apply gamma correction and show the images
+        gamma = gamma if gamma > 0 else 0.1
+        adjusted = adjust_gamma(original, gamma=gamma)
+    return adjusted
 
 
 def crop_face_transform(image):
