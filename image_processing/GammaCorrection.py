00a2757bb43a25474032e50cd8c1c13e4f73e463
MuhammadHussain3 <hmuhammad9259@bths.edu>
2020-04-10 18:19:13 -0400

    added to Image Processing- Gamma correction function

--- image_processing/GammaCorrection.py
+++ image_processing/GammaCorrection.py
@@ -1,5 +1,6 @@
 # USAGE
-# python adjust_gamma.py --image example_01.png
+# python GammaCorrection.py --image example_01.png
+# python GammaCorrection.py --image StonyBrook.jpg
 
 # import the necessary packages
 from __future__ import print_function
@@ -8,16 +9,16 @@ import argparse
 import cv2
 
 
-def gamma_correction(image):
-    def adjust_gamma(image, gamma=1.0):
-        # build a lookup table mapping the pixel values [0, 255] to
-        # their adjusted gamma values
-        invGamma = 1.0 / gamma
-        table = np.array([((i / 255.0) ** invGamma) * 255
-                          for i in np.arange(0, 256)]).astype("uint8")
+# def gamma_correction(image):
+def adjust_gamma(image, gamma=1.0):
+    # build a lookup table mapping the pixel values [0, 255] to
+    # their adjusted gamma values
+    invGamma = 1.0 / gamma
+    table = np.array([((i / 255.0) ** invGamma) * 255
+                      for i in np.arange(0, 256)]).astype("uint8")
 
-        # apply gamma correction using the lookup table
-        return cv2.LUT(image, table)
+    # apply gamma correction using the lookup table
+    return cv2.LUT(image, table)
 
     # construct the argument parse and parse the arguments
     ap = argparse.ArgumentParser()
@@ -29,7 +30,7 @@ def gamma_correction(image):
     original = cv2.imread(args["image"])
 
     # loop over various values of gamma
-    for gamma in np.arange(0.0, 3.5, 0.5):
+    for gamma in np.arange(1.0, 3.0, 1.0):
         # ignore when gamma is 1 (there will be no change to the image)
         if gamma == 1:
             continue
@@ -37,8 +38,4 @@ def gamma_correction(image):
         # apply gamma correction and show the images
         gamma = gamma if gamma > 0 else 0.1
         adjusted = adjust_gamma(original, gamma=gamma)
-    return adjusted
-    # cv2.putText(adjusted, "g={}".format(gamma), (10, 30),
-    # cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
-    # cv2.imshow("Images", np.hstack([original, adjusted]))
-    # cv2.waitKey(0)
+        return adjusted
--- image_processing/ImgProcessing.py
+++ image_processing/ImgProcessing.py
@@ -10,6 +10,7 @@ from __future__ import print_function
 import numpy as np
 import argparse
 import cv2
+from GammaCorrection import adjust_gamma
 
 
 def crop_faces(image):
@@ -24,7 +25,7 @@ def crop_faces(image):
     crops = []
     for f in faces:
         x, y, w, h = [v for v in f]
-        sub_face = opencv_img[y:y+h, x:x+h]
+        sub_face = opencv_img[y:y + h, x:x + h]
         sub_face = cv2.cvtColor(sub_face, cv2.COLOR_BGR2RGB)
         crop = {
             "coord": (x, y),
@@ -48,40 +49,9 @@ def histogram_equalization(image):
     return img_output
 
 
-def gamma_correction(image):
-    def adjust_gamma(gamma=1.0):
-        # build a lookup table mapping the pixel values [0, 255] to
-        # their adjusted gamma values
-        invGamma = 1.0 / gamma
-        table = np.array([((i / 255.0) ** invGamma) * 255
-                          for i in np.arange(0, 256)]).astype("uint8")
-
-        # apply gamma correction using the lookup table
-        return cv2.LUT(image, table)
-
-    # construct the argument parse and parse the arguments
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-i", "--image", required=True,
-                    help="path to input image")
-    args = vars(ap.parse_args())
-
-    # load the original image
-    original = cv2.imread(args["image"])
-
-    # loop over various values of gamma
-    for gamma in np.arange(0.0, 3.5, 0.5):
-        # ignore when gamma is 1 (there will be no change to the image)
-        if gamma == 1:
-            continue
-
-        # apply gamma correction and show the images
-        gamma = gamma if gamma > 0 else 0.1
-        adjusted = adjust_gamma(original, gamma=gamma)
-    return adjusted
-
-
 def crop_face_transform(image):
     faces = crop_faces(image)
+    faces = adjust_gamma(faces, gamma=1.0)
     if len(faces) == 0:
         return image
     return faces[0]["img"]
