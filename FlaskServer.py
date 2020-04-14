311a992ad92ab7f93a0ffc1e6f0536f37a3e2a57
John Boccio <john.t.boccio@gmail.com>
2020-04-13 21:36:31 -0400

    Added FPS tracker, made it so image gets updated on website as fast as possible

--- FlaskServer.py
+++ FlaskServer.py
@@ -47,70 +47,87 @@ if torch.cuda.is_available():
 app = Flask(__name__)
 
 
-def fer_processor():
+def fer_processor(print_interval=-1, log=None):
     global image, image_lock, image_event, fer_processing, fer_processing_lock, fer_processing_event
 
+    fps_tracker = utils.FpsTracker(print_interval=print_interval, log=log)
     while True:
         # Wait for a new image to process from video_feed
         image_event.wait()
         image_event.clear()
+        fps_tracker.track()
 
         with image_lock:
             # Image data is sent as string from FlaskSend.py
             np_img = np.frombuffer(image['data'], np.uint8)
             cropped = image['cropped']
-            size = image['size']
 
         # Process the image and make it available for the fer_generator
         np_img = cv2.imdecode(np_img, cv2.IMREAD_COLOR)
         if not cropped:
             face = image_processing.crop_face_transform(np_img)
-            x, y = face["coord"]
-            w, h = face["size"]
-            np_img = cv2.rectangle(np_img, (x, y), (x + w, y + h), (0, 0, 255), 1)
             pil_img = Image.fromarray(face['img'])
             transformed_img = vgg_transform(pil_img)
             expression, exp_pdist = utils.get_expression(model, transformed_img, need_softmax=True)
-            np_img = cv2.putText(np_img, expression, (max(x, 10), max(y-5, 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
         else:
+            face = None
             pil_img = Image.fromarray(np_img)
             transformed_img = vgg_transform(pil_img)
             expression, exp_pdist = utils.get_expression(model, transformed_img, need_softmax=True)
-            np_img = cv2.putText(np_img, str(expression), (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
 
-        np_img = cv2.resize(np_img, (size[0], size[1]))
+        with fer_processing_lock:
+            # Update fer_processing with the new information for fer_generator to use
+            fer_processing['exp'] = expression
+            fer_processing['exp_pdist'] = exp_pdist
+            fer_processing['face'] = face
+        # Let fer_generator know of the new data
+        fer_processing_event.set()
 
-        success, img = cv2.imencode(".jpg", np_img, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
-        if success:
-            with fer_processing_lock:
-                # Update fer_processing with the new information for fer_generator to use
-                fer_processing['img'] = img
-                fer_processing['exp'] = expression
-                fer_processing['exp_pdist'] = exp_pdist
-            # Let fer_generator know of the new data
-            fer_processing_event.set()
+        fps_tracker.frame_sent()
 
 
 def fer_generator():
     global image, image_lock
 
+    fer_data = None
+
     # Continuously loop over the frames received from /video_feed
     while True:
         # sleep until a new processed image comes
-        fer_processing_event.wait()
-        fer_processing_event.clear()
+        image_event.wait()
+        image_event.clear()
 
+        with image_lock:
+            # Image data is sent as string from FlaskSend.py
+            img = np.frombuffer(image['data'], np.uint8)
+            cropped = image['cropped']
+
+        img = cv2.imdecode(img, cv2.IMREAD_COLOR)
         # Acquire lock for fer processing information and get the information
-        with fer_processing_lock:
-            img = fer_processing['img']
-            expression = fer_processing['exp']
-            expression_pdist = fer_processing['exp_pdist']
+        if fer_processing_event.is_set():
+            with fer_processing_lock:
+                fer_data = fer_processing
+            fer_processing_event.clear()
+            print(f"Expression: {fer_data['exp']}\tProbabilities: {fer_data['exp_pdist']}")
 
-        print(f"Expression: {expression}")
-        print(f"Expression probability distribution: {expression_pdist}")
+        if fer_data is not None:
+            if not cropped:
+                face = fer_data['face']
+                x, y = face['coord']
+                w, h = face['size']
+                img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 1)
+                img = cv2.putText(img, fer_data['exp'], (max(x, 10), max(y-5, 10)),
+                                              cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+            else:
+                img = cv2.putText(img, str(fer_data['exp']), (10, 10),
+                                              cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
 
-        # yield the output frame in the byte format
-        yield b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(img) + b'\r\n'
+        img = cv2.resize(img, (args.size[0], args.size[1]))
+
+        success, img = cv2.imencode(".jpg", img, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+        if success:
+            # yield the output frame in the byte format
+            yield b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(img) + b'\r\n'
 
 
 @app.route("/", methods=['GET'])
@@ -137,15 +154,20 @@ def video_feed_receive(cropped, width, height):
     return Response(response=response, status=200, mimetype="application/json")
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Host server to receive images')
-    parser.add_argument('--host', dest='host', default=None, help="Web address to host the server")
-    parser.add_argument('--port', dest='port', default=None, help="Port to host the server")
-    parser.add_argument('--debug', dest='debug', action='store_true', help='Set server to debug mode')
-    args = parser.parse_args()
+parser = argparse.ArgumentParser(description='Host server to receive images')
+parser.add_argument('--host', dest='host', default=None, help="Web address to host the server")
+parser.add_argument('--port', dest='port', default=None, help="Port to host the server")
+parser.add_argument('--debug', dest='debug', action='store_true', help='Set server to debug mode')
+parser.add_argument('--print', dest='print', type=int, default=-1, help='Set the print interval for FER FPS statistics')
+parser.add_argument('--log', dest='log', default=None, help='File to log raw FPS data to')
+parser.add_argument('--size', dest='size', nargs=2, type=int, default=[720, 480], metavar=('width', 'height'),
+                    help='Specify what size image to show on the front-end (default: 720 x 480)')
+args = parser.parse_args()
 
+
+if __name__ == '__main__':
     # Start the thread that will be doing the facial expression recognition on the received images
-    fer_processing_thread = threading.Thread(target=fer_processor, daemon=True)
+    fer_processing_thread = threading.Thread(target=fer_processor, args=(args.print, args.log), daemon=True)
     fer_processing_thread.start()
 
     app.run(host=args.host, port=args.port, debug=args.debug)
---
+++ utils/FpsTracker.py
@@ -0,0 +1,80 @@
+import time
+
+
+class FpsTracker(object):
+    def __init__(self, fps_limit=-1, average_over=10, print_interval=-1, log=None, flush_interval=1):
+        """
+        FpsTracker allows you to track the amount of frames you have sent and limit your FPS. The FPS is calculated by
+        using a moving average over n samples.
+
+        :param fps_limit: specify the maximum FPS you'd like to process, if <= 0 then it will run as fast as possible
+        :param average_over: number of samples to use for moving average
+        :param print_interval: frame_sent will print the current fps statistics if this amount of time in ms has passed
+            since the last time it printed. Use -1 if you would not like to print.
+        :param log: file to log raw FPS data to, will not perform logging if 'None'.
+        """
+        self.fps_limit = fps_limit if fps_limit > 0 else -1
+        self.average_over = average_over
+        self.print_interval = print_interval
+        self.last_print_time = 0
+        self.last_frame_sent_time = 0
+        self.fps = 0
+        self.moving_avg = []
+        self.frames_sent = 0
+        self.frames_per_ms = self.fps_limit / 1000
+        self.ms_per_frame = 1 / self.frames_per_ms
+        self.log_file = log
+        self.log = None if log is None else open(log, 'w')
+        self.flush_interval = flush_interval
+
+        self.current_time_ms = lambda: int(round(time.time()*1000))
+
+    def frame_sent(self):
+        """
+        Call after sending a frame. Each frame is allotted 'self.ms_per_frame' amount of time to send. If the frame has
+        been sent in less than that amount of time, it will sleep the remainder.
+        """
+        self.frames_sent += 1
+
+        current_time = self.current_time_ms()
+        time_elapsed = current_time - self.last_frame_sent_time
+
+        if time_elapsed < self.ms_per_frame:
+            # Sleep the remainder of the given time
+            seconds_remaining = (self.ms_per_frame - time_elapsed) / 1000
+            time.sleep(seconds_remaining)
+        tmp_time = self.last_frame_sent_time
+        self.last_frame_sent_time = self.current_time_ms()
+
+        instantaneous_fps = (1 / (self.current_time_ms() - tmp_time)) * 1000
+        self.moving_avg.append(instantaneous_fps)
+        if self.frames_sent < self.average_over:
+            self.fps = (self.fps * (self.frames_sent - 1) + instantaneous_fps) / self.frames_sent
+        else:
+            self.fps = sum(self.moving_avg) / self.average_over
+            self.moving_avg.pop(0)
+
+        if self.log is not None:
+            self.log.write(f"{instantaneous_fps:4.4f}, {self.fps:4.4f}\r\n")
+            if (self.frames_sent % self.flush_interval) == 0:
+                self.log.flush()
+
+        if self.print_interval >= 0:
+            time_elapsed = current_time - self.last_print_time
+            if time_elapsed > self.print_interval:
+                print(f"Frames sent: {self.frames_sent:6d}\tFPS: {self.fps:4.4f}")
+                self.last_print_time = self.current_time_ms()
+
+    def track(self):
+        self.last_frame_sent_time = self.current_time_ms()
+        self.last_print_time = self.current_time_ms()
+
+    def reset_fps(self):
+        self.fps = 0
+        self.frames_sent = 0
+        self.moving_avg = []
+
+    def close_log(self):
+        if self.log is not None:
+            self.log.close()
+
